0e3233869466cb95d841556763496305136782d5
Bako19 <104640261+Bako19@users.noreply.github.com>
2022-09-08 14:25:24 +0200

    1

---
+++ js/script.js
@@ -0,0 +1,37 @@
+try {
+    const animationDiv = document.querySelectorAll(".animationDiv")
+    const imgs = document.querySelectorAll(".animated")
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            let ind = Array.from(animationDiv).indexOf(entry.target)
+            if (entry.isIntersecting) {
+                if (imgs[ind].classList[0] == "animation1") {
+                    imgs[ind].classList.add("fadeInUp")
+                }
+                if (imgs[ind].classList[0] == "animation2") {
+                    imgs[ind].classList.add("fadeInRight")
+                }
+                if (imgs[ind].classList[0] == "animation3") {
+                    imgs[ind].classList.add("fadeInLeft")
+                }
+                if (imgs[ind].classList[0] == "animation4") {
+                    imgs[ind].classList.add("fadeIn")
+                }
+            } else {
+                if (entry.boundingClientRect.y > 0) {
+                    imgs[ind].classList.remove("fadeInUp")
+                    imgs[ind].classList.remove("fadeInRight")
+                    imgs[ind].classList.remove("fadeInLeft")
+                    imgs[ind].classList.remove("fadeIn")
+                }
+            }
+        })
+    }, {
+        threshold: 0,
+    })
+    animationDiv.forEach(fadeInUpDiv => {
+        observer.observe(fadeInUpDiv)
+    })
+} catch (error) {
+
+}

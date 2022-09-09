c662051d5fd4d83aef39eab22becbfb54a58749e
Bako19 <104640261+Bako19@users.noreply.github.com>
2022-09-10 00:41:21 +0200

    5

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
-function display() {
+function display(sold) {
     console.log("Done")
-    document.getElementById("sold").classList.add("dis")
+    document.getElementById(sold).classList.add("dis")
 }
 
-function disdisplay() {
+function disdisplay(sold) {
     console.log("Done")
-    document.getElementById("sold").classList.remove("dis")
+    document.getElementById(sold).classList.remove("dis")
 }
 
 sleep = milliseconds => new Promise(resolve => setTimeout(resolve, milliseconds));
+const d = new Date();
+
+
 async function closeBtn() {
     document.getElementById("window").classList.add("windonone")
     await sleep(800);
     document.getElementById("window").classList.add("d-none")
+    localStorage.setItem("day",d.getDate())
 }
 addEventListener('load', (event) => {
-    console.log("BOOOOOOOOOOOOOM")
-    document.getElementById("window").classList.remove("d-none")
+    if (!(localStorage.getItem("day") ==  d.getDate())) {
+        document.getElementById("window").classList.remove("d-none")
+    }
 });

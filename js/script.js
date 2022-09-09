83bab343c56509de7553bbd57d096eebe6531dfe
Bako19 <104640261+Bako19@users.noreply.github.com>
2022-09-09 19:40:10 +0200

    mm

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 function display() {
+    console.log("Done")
     document.getElementById("sold").classList.add("dis")
 }
 
 function disdisplay() {
+    console.log("Done")
     document.getElementById("sold").classList.remove("dis")
 }
+
+sleep = milliseconds => new Promise(resolve => setTimeout(resolve, milliseconds));
+async function closeBtn() {
+    document.getElementById("window").classList.add("windonone")
+    await sleep(800);
+    document.getElementById("window").classList.add("d-none")
+}
+addEventListener('load', (event) => {
+    console.log("BOOOOOOOOOOOOOM")
+    document.getElementById("window").classList.remove("d-none")
+});
